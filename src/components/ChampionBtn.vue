37174e43acc62c35623d50e078a0d51dc75e144b
johnlin <john830110@gmail.com>
2022-10-31 01:02:40 +0800

    feat: 按下按鈕可以來回改變顏色

--- src/components/ChampionBtn.vue
+++ src/components/ChampionBtn.vue
@@ -1,19 +1,22 @@
 <template>
-  <!-- <div style="background: #000000"> -->
-  <div v-if="showname" style="width: 120px">
-    <q-btn round @click="test">
-      <q-avatar size="42px">
-        <!-- <img src="~assets/champion/Aatrox.png" /> -->
+  <div v-if="showname" style="width: 150px">
+    <q-btn
+      @click="test"
+      stack
+      :color="btnColor"
+      :text-color="textColor"
+      style="width: 150px"
+    >
+      <q-avatar>
         <img :src="path" />
       </q-avatar>
+      <a>{{ champion }}</a>
     </q-btn>
-    <a>{{ champion }}</a>
   </div>
 
   <div v-else>
-    <q-btn round @click="test">
+    <q-btn @click="test" :color="btnColor" :text-color="textColor">
       <q-avatar size="42px">
-        <!-- <img src="~assets/champion/Aatrox.png" /> -->
         <img :src="path" />
       </q-avatar>
     </q-btn>
@@ -22,14 +25,31 @@
 </template>
 
 <script setup>
-// import { props } from "vue";
+import { ref } from "vue";
+const btnColor = ref("white");
+const textColor = ref("black");
 const props = defineProps({
+  num: Number,
   champion: String,
   showname: Boolean,
 });
 
+const emit = defineEmits(["update"]);
+const changeColor = () => {
+  if (btnColor.value === "white") {
+    btnColor.value = "purple";
+    textColor.value = "white";
+  } else {
+    btnColor.value = "white";
+    textColor.value = "black";
+  }
+};
+
 const test = () => {
   console.log(props.champion);
+  console.log(props.num);
+  emit("update", props.num);
+  changeColor();
 };
 
 const filename = props.champion
