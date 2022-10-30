60eef51d2fb7d98e3934a5f48cb7db74b90a357c
johnlin <john830110@gmail.com>
2022-10-31 01:01:50 +0800

    feat: 調整按鈕位置

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,22 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="q-pa-md row items-start q-gutter-md" style="width: 80vw">
-      <!-- <div class="row items-start" style="width: 80vw"> -->
-      <ChampionBtn
-        v-for="(item, index) in champion_list"
-        :key="index"
-        :champion="item.name"
-        :showname="false"
-      ></ChampionBtn>
+    <div class="column">
+      <div class="col">
+        <q-toggle v-model="showname" label="Show name" />
+      </div>
+      <div class="col">
+        <div class="q-pa-md row items-start q-gutter-md" style="width: 80vw">
+          <!-- <div class="row items-start" style="width: 80vw"> -->
+          <ChampionBtn
+            v-for="(item, index) in champion_list"
+            :key="index"
+            :num="index"
+            :champion="item.name"
+            :showname="showname"
+            @update="updateList"
+          ></ChampionBtn>
+        </div>
+      </div>
     </div>
   </q-page>
 </template>
@@ -15,7 +24,13 @@
 <script setup>
 import ChampionBtn from "src/components/ChampionBtn.vue";
 import { champion_list } from "src/assets/champion_list";
+import { ref } from "vue";
 
-console.log(champion_list[0].name_chinese);
-console.log(123);
+const showname = ref(false);
+let record = Array(161).fill(-1);
+
+const updateList = (num) => {
+  console.log("qqq", num);
+  record[num] = record[num] * -1;
+};
 </script>
