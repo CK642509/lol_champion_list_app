96d053746fc5002d6999d2b5cb1b4f68cff3877d
johnlin <john830110@gmail.com>
2023-01-10 00:08:27 +0800

    feat: 新增初始化資料

--- src/utils/sequelize.js
+++ src/utils/sequelize.js
@@ -1,9 +1,11 @@
 import { Sequelize, DataTypes } from "sequelize";
 import sqlite3 from "sqlite3";
 
+import { champion_list } from "../assets/champion_list";
+
 let sequelize;
 let Champions;
-let Record;
+let Record_1;
 let Pages;
 
 const init_db = (path) => {
@@ -16,7 +18,11 @@ const init_db = (path) => {
   Champions = sequelize.define(
     "champions",
     {
-      id: DataTypes.INTEGER,
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
       name_en: DataTypes.STRING,
       name_ch: DataTypes.STRING,
       date: DataTypes.DATE,
@@ -26,17 +32,56 @@ const init_db = (path) => {
     }
   );
 
-  Record = sequelize.define("record", {
-    champion: DataTypes.STRING,
-    state: DataTypes.BOOLEAN,
-  });
+  Record_1 = sequelize.define(
+    "record_1",
+    {
+      champ_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "champions",
+          key: "id",
+        },
+      },
+      champion: DataTypes.STRING,
+      state: DataTypes.BOOLEAN,
+    },
+    {
+      freezeTableName: true,
+    }
+  );
 
   Pages = sequelize.define("pages", {
-    id: DataTypes.INTEGER,
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
     title: DataTypes.STRING,
   });
 
   sequelize.sync();
+};
+
+const init_champ = async () => {
+  for (let i = 0; i < champion_list.length; i++) {
+    await Champions.create({
+      id: i + 1,
+      name_en: champion_list[i].name,
+      name_ch: champion_list[i].name_chinese,
+      date: champion_list[i].date,
+    }).catch((error) => {
+      console.log(error.message);
+    });
+  }
+};
+
+const init_page = async () => {
+  await Pages.create({
+    id: 1,
+    title: "S-",
+  }).catch((error) => {
+    console.log(error.message);
+  });
 };
 
 // TODO: User.findOrCreate
@@ -48,4 +93,4 @@ const load_record = async () => {
   //
 };
 
-export { init_db, save_record, load_record };
+export { init_db, init_champ, init_page, save_record, load_record };
